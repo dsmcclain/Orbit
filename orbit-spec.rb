a728f499b3504d1a0d835ee71db9d57310dee878
Dylan McClain <dylan.mcclain@gmail.com>
2019-06-29 10:38:04 -0700

    add gitignore for testing files

--- orbit-spec.rb
+++
@@ -1,9 +0,0 @@
-#This test file depends on RSpec version 3.8
-require_relative 'orbit.rb'
-
-RSpec.describe Game do
-  it "initializes astronauts correctly" do
-    game = Game.new(astronaut_generator(3), Turn.new(Map.new))
-    expect(game.astronauts.size).to eq 3
-  end
-end
